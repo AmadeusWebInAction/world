8f996afcd5859e7382950ecd356ca890b697ee49
Imran Ali Namazi <imran@amadeusweb.com>
2025-03-31 07:22:37 +0530

    few nodes added, i am now officially a "lightworker"

--- cms.php
+++ cms.php
@@ -1,5 +1,18 @@
 <?php
 variables([
+	'link-to-site-home' => true,
 	'link-to-section-home' => true,
 	
 ]);
+
+function enrichThemeVars($vars, $what) {
+	if ($what == 'header' && variable('node') == 'index') {
+		$vars['optional-slider'] = getSnippet('home-slider');
+	}
+
+	if (function_exists('enrichNodeThemeVars'))
+		$vars = enrichNodeThemeVars($vars, $what);
+
+	return $vars;
+}
+
--- index.php
+++ index.php
@@ -5,16 +5,4 @@ ini_set('display_errors', '1');
 define('SITEPATH', __DIR__);
 include_once '../core/framework/1-entry.php';
 
-if (variable('node') == 'index') {
-	variable('sub-theme', 'splash');
-}
-
-function enrichThemeVars($vars, $what) {
-	if ($what == 'header' && variable('node') == 'index') {
-		$vars['optional-slider'] = getSnippet('home-slider');
-	}
-
-	return $vars;
-}
-
 runFrameworkFile('site');
